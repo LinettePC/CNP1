7e34aedc0968f3b0f9c7555e24cfa79af9815bbe
LinettePC <lperezc@ucenfotec.ac.cr>
2024-03-30 15:56:03 -0600

    continuar con validaciones registroCliente

--- public/js/registroCliente.js
+++ public/js/registroCliente.js
@@ -1,5 +1,40 @@
+
+//alert("Si se ligo el JS para validaciones :) !!!")
 //Referencia al DOM
-const identificacionCliente = document.getElementById("identificacion")
+const idCliente = document.getElementById("identificacion")
+const nombre = document.getElementById("nombre")
+const primerApellido = document.getElementById("primerApellido")
+const segundoApellido = document.getElementById("segundoApellido")
+const correo = document.getElementById("correo")
+const telefono = document.getElementById("telefono")
+const contrasenna = document.getElementById("contrasenna")
+
+//crear una funcion para validar campos vacios
+function validarCamposVacios(){
+    //recordar que "segundoApellido" NO es requerido 
+    let camposRequeridos = document.querySelectorAll("#formularioCuentaCliente [required]")
+    let error = false
+
+    for (let i=0; i< camposRequeridos.length;i++){
+        if(camposRequeridos[i].value==""){
+            error=true
+        }
+
+    }
+
+
+
+
+
+}
+
+
 
 //validar identificacion
-funt
+function validarNombre(){
+
+}
+
+
+function validarIdentificacion(){
+}
