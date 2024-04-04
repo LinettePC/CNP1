712a5c2c9a97eca367e316764835217c700191f8
Txaverria <isaac.chc24@gmail.com>
2024-04-03 20:32:56 -0600

    Updates to "models" folder. Get user with id added

--- api/routes/personas.js
+++ api/routes/personas.js
@@ -1,6 +1,6 @@
 const express = require('express');
 //necesitamos requerir el modelo de personas
-const Persona = require('../modelos/personas');
+const Persona = require('../models/personas');
 const router = express.Router();
 
 //http://localhost:3000/api/listar
@@ -26,8 +26,8 @@ router.get('/listar', (req, res) => {
 // http://localhost:3000/api/buscar-persona-nombre
 // Endpoint para agarrar un usuario específico
 router.get('/buscar-persona-nombre', (req, res) => {
-	let requestedName = req.query.nombre;
-	Persona.find({ nombre: requestedName }, (error, personaBuscada) => {
+	let requestedNombre = req.query.nombre;
+	Persona.find({ nombre: requestedNombre }, (error, personaBuscada) => {
 		if (error) {
 			res.status(501).json({
 				resultado: false,
@@ -41,8 +41,33 @@ router.get('/buscar-persona-nombre', (req, res) => {
 				res.json({
 					resultado: true,
 					msj: 'Usuario encontrado:',
-                    persona:personaBuscada,
-                });
+					persona: personaBuscada,
+				});
+			}
+		}
+	});
+});
+
+// http://localhost:3000/api/buscar-persona-cedula
+// Endpoint para agarrar un usuario específico
+router.get('/buscar-persona-cedula', (req, res) => {
+	let requestedCedula = req.query.cedula;
+	Persona.find({ cedula: requestedCedula }, (error, personaBuscada) => {
+		if (error) {
+			res.status(501).json({
+				resultado: false,
+				msj: 'Ocurrió el siguiente error:',
+				error,
+			});
+		} else {
+			if (personaBuscada == '') {
+				res.json({ msj: 'La persona no existe.' });
+			} else {
+				res.json({
+					resultado: true,
+					msj: 'Usuario encontrado:',
+					persona: personaBuscada,
+				});
 			}
 		}
 	});
