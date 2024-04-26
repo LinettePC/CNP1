bbf054258c99950e4509c70eb1fef962c5b81c92
LinettePC <lperezc@ucenfotec.ac.cr>
2024-04-25 22:50:44 -0600

    Update registroVendedor.js

--- public/js/registroVendedor.js
+++ public/js/registroVendedor.js
@@ -164,17 +164,19 @@ function principal() {
 			text: 'Recibirás un mensaje cuando tu solicitud sea revisada',
 			icon: 'success'
 		});
-		cedula = idCliente.value
-		nombre = nombre.value
-		primerApellido = primerApellido.value
-		nomTramo = nomTramo.value
-		correo = correo.value
-		telefono = telefono.value
-		permiso = permiso.value
-		foto = foto.value
-		contrasenna = contrasenna.value
+
+
+		let cedula = idCliente.value
+		let nombre = nombre.value
+		let primerApellido = primerApellido.value
+		let nomTramo = nomTramo.value
+		let correo = correo.value
+		let telefono = telefono.value
+		let permiso = permiso.value
+		let foto = foto.value
+		let contrasenna = contrasenna.value
 		
-		// //preRegistroVendedor(idCliente.value,nombre.value,primerApellido.value,nomTramo.value,correo.value,telefono.value,permiso.value,foto.value)
+		//preRegistroVendedor(idCliente.value,nombre.value,primerApellido.value,nomTramo.value,correo.value,telefono.value,permiso.value,foto.value)
 		
 		preRegistroVendedor(cedula,nombre,primerApellido,nomTramo,correo,telefono,permiso,foto)
 		limpiarCampos();
