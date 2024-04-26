889daa42f00278bd369b0734bae158d956581ba8
Sebastian Sanchez <120608697+Sebasarthur@users.noreply.github.com>
2024-04-26 16:28:43 -0600

    prueba

--- public/js/marketplace.js
+++ public/js/marketplace.js
@@ -4,6 +4,6 @@ const cantidadItemsCarrito = document.getElementById("cantidadItemsCarrito");
 
 document.addEventListener("DOMContentLoaded", async () => {
     listaObjetosLocalStorage = JSON.parse(localStorage.getItem("productos_en_carrito")) || [];
-     cantidadItemsCarrito.innerHTML = listaObjetosLocalStorage.length;
+    cantidadItemsCarrito.innerHTML = listaObjetosLocalStorage.length;
   });
 
