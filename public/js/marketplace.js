93b908da2b2437808738c1873bbae0c472c25df8
sgarital <sgarital@ucenfotec.ac.cr>
2024-05-01 21:11:05 -0600

    nombre de cliente logueado y otros arreglos

--- public/js/marketplace.js
+++ public/js/marketplace.js
@@ -1,8 +1,10 @@
 const cantidadItemsCarrito = document.getElementById("cantidadItemsCarrito");
-
+const nombreCliente = document.getElementById("textoCuenta")
 
 
 document.addEventListener("DOMContentLoaded", async () => {
+    usuarioActual = await conseguirCompradorCedula(sessionStorage.getItem('cedula'));
+    nombreCliente.innerText = `${usuarioActual.nombre} ${usuarioActual.primerApellido}`;
     listaObjetosLocalStorage = JSON.parse(localStorage.getItem("productos_en_carrito")) || [];
     cantidadItemsCarrito.innerHTML = listaObjetosLocalStorage.length;
   });
