6f45e90b3e767dd17c05f9130e5d290660de41bb
Isaac Ch <89037579+Txaverria@users.noreply.github.com>
2024-12-05 22:54:20 -0600

    Update ventas.js to include Redis

--- api/routes/ventas.js
+++ api/routes/ventas.js
@@ -1,99 +1,96 @@
-const express = require('express');
+const express = require("express");
 //necesitamos requerir el modelo de Clientes
-const Venta = require('../models/ventas');
+const Venta = require("../models/ventas");
 const router = express.Router();
 
 function conseguirFechaFormateada() {
-	let fechaActual = new Date(); // 4/12/2024::9:06PM
-	let dia = fechaActual.getDate();
-	let mes = fechaActual.getMonth() + 1; // Enero es 0
-	let anno = fechaActual.getFullYear();
+  let fechaActual = new Date(); // 4/12/2024::9:06PM
+  let dia = fechaActual.getDate();
+  let mes = fechaActual.getMonth() + 1; // Enero es 0
+  let anno = fechaActual.getFullYear();
 
-	// Agrega los 0's para el formato
-	dia = dia < 10 ? '0' + dia : dia;
-	mes = mes < 10 ? '0' + mes : mes;
+  // Agrega los 0's para el formato
+  dia = dia < 10 ? "0" + dia : dia;
+  mes = mes < 10 ? "0" + mes : mes;
 
-	return `${dia}/${mes}/${anno}`;
+  return `${dia}/${mes}/${anno}`;
 }
 
 //http://localhost:3000/api/registrar-venta
-router.post('/registrar-venta', (req, res) => {
-	let body = req.body;
+router.post("/registrar-venta", (req, res) => {
+  let body = req.body;
 
-	let fechaFormateada = conseguirFechaFormateada();
+  let fechaFormateada = conseguirFechaFormateada();
 
-	let nueva_Venta = new Venta(body);
+  let nueva_Venta = new Venta(body);
 
-	nueva_Venta.fecha_de_venta = body.fecha_de_venta ? body.fecha_de_venta : fechaFormateada;
+  nueva_Venta.fecha_de_venta = body.fecha_de_venta ? body.fecha_de_venta : fechaFormateada;
 
-	nueva_Venta.save((error, VentaDB) => {
-		if (error) {
-			res.status(500).json({
-				resultado: false,
-				msj: 'No se pudo hacer el registro de la venta',
-				error,
-			});
-		} else {
-			res.status(200).json({
-				resultado: true,
-				msj: 'Registro exitoso de venta',
-				VentaDB,
-			});
-		}
-	});
+  nueva_Venta.save((error, VentaDB) => {
+    if (error) {
+      res.status(500).json({
+        resultado: false,
+        msj: "No se pudo hacer el registro de la venta",
+        error,
+      });
+    } else {
+      res.status(200).json({
+        resultado: true,
+        msj: "Registro exitoso de venta",
+        VentaDB,
+      });
+    }
+  });
 });
 
 //http://localhost:3000/api/listar-ventas
 //GET--> recuperar informacion
-router.get('/listar-ventas', (req, res) => {
-	Venta.find((error, lista) => {
-		if (error) {
-			res.status(500).json({
-				resultado: false,
-				msj: 'No se pudieron listar las ventas',
-				error,
-			});
-		} else {
-			res.status(200).json({
-				resultado: true,
-				msj: 'Listado exitosos',
-				lista,
-			});
-		}
-	});
+router.get("/listar-ventas", (req, res) => {
+  Venta.find((error, lista) => {
+    if (error) {
+      res.status(500).json({
+        resultado: false,
+        msj: "No se pudieron listar las ventas",
+        error,
+      });
+    } else {
+      res.status(200).json({
+        resultado: true,
+        msj: "Listado exitosos",
+        lista,
+      });
+    }
+  });
 });
 
 //http://localhost:3000/api/listar-ventas-cedula
 //GET--> recuperar informacion
-router.get('/listar-ventas-cedula', (req, res) => {
-	let requestedCedula = req.query.cedula;
-	Venta.find(
-		{
-			$or: [
-				{ cedula_comprador: requestedCedula },
-				{ cedula_vendedor: requestedCedula },
-			],
-		},
-		(error, VentaBuscada) => {
-			if (error) {
-				res.status(501).json({
-					resultado: false,
-					msj: 'Ocurrió el siguiente error:',
-					error,
-				});
-			} else {
-				if (VentaBuscada.length === 0) {
-					res.json({ msj: 'La venta no existe.' });
-				} else {
-					res.json({
-						resultado: true,
-						msj: 'Venta encontrada:',
-						listaVentas: VentaBuscada,
-					});
-				}
-			}
-		}
-	);
+router.get("/listar-ventas-cedula", (req, res) => {
+  let requestedCedula = req.query.cedula;
+  Venta.find(
+    {
+      $or: [{ cedula_comprador: requestedCedula }, { cedula_vendedor: requestedCedula }],
+    },
+    (error, VentaBuscada) => {
+      if (error) {
+        res.status(501).json({
+          resultado: false,
+          msj: "Ocurrió el siguiente error:",
+          error,
+        });
+      } else {
+        if (VentaBuscada.length === 0) {
+          res.json({ msj: "La venta no existe." });
+        } else {
+          res.json({
+            resultado: true,
+            msj: "Venta encontrada:",
+            listaVentas: VentaBuscada,
+          });
+        }
+      }
+    }
+  );
 });
 
 // http://localhost:3000/api/buscar-Cliente-cedula
